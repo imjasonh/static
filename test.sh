ad6e9b2e7a0e5c017c3b1ed7c30f1218a7a44179
Jason Hall <jason@chainguard.dev>
2022-10-14 14:16:55 -0400

    try to avoid permission denied on go build
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/static"}
 
 for lang in c golang rust; do
   docker build --build-arg BASE=${IMAGE_NAME} --tag smoke-test-${lang} --file examples/Dockerfile.${lang} examples
-  docker run smoke-test-${lang}
+  docker run --rm smoke-test-${lang}
 done
